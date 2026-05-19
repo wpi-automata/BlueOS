27503b7c955a17697b458dc7ebe1a37168d0b5b6
Sam Honor <shonor47@gmail.com>
2026-05-18 23:43:24 -0400

    Implemented sourcery feedback to sanitize user input to endpoint argument

--- core/services/ardupilot_manager/autopilot_manager.py
+++ core/services/ardupilot_manager/autopilot_manager.py
@@ -25,6 +25,7 @@ from mavlink_proxy.exceptions import EndpointAlreadyExists
 from mavlink_proxy.Manager import Manager as MavlinkManager
 from settings import Settings
 from typedefs import (
+    EndpointDefinition,
     Firmware,
     FlightController,
     FlightControllerFlags,
@@ -439,6 +440,7 @@ class AutoPilotManager(metaclass=Singleton):
         self.ardupilot_subprocess = None
         await self.start_mavlink_manager(self.master_endpoint)
 
+    # pylint: disable=too-many-locals
     async def start_sitl(self) -> None:
         self._current_board = BoardDetector.detect_sitl()
         if not self.firmware_manager.is_firmware_installed(self._current_board):
@@ -463,25 +465,39 @@ class AutoPilotManager(metaclass=Singleton):
         arguments = self.configuration["exec_arguments"][str(firmware_path)]["SITL"]
 
         # ArduPilot SITL binary will bind TCP port 5760 (server) and the mavlink router will connect to it as a client
-        if "endpoint" not in arguments:
-            logger.warning("Using default endpoint for SITL because none was provided in settings.json.")
-            master_endpoint = Endpoint(
-                name="Master",
-                owner=self.settings.app_name,
-                connection_type=EndpointType.TCPClient,
-                place="127.0.0.1",
-                argument=5760,
-                protected=True,
-            )
-        else:
-            master_endpoint = Endpoint(**arguments["endpoint"])
+        default_endpoint_args = EndpointDefinition()
 
-        arg_list = [firmware_path]
+        endpoint_config = arguments.get("endpoint")
+        if endpoint_config is None:
+            logger.warning("Using default endpoint for SITL because none was provided in settings.json.")
+            master_endpoint_args = default_endpoint_args
+        else:
+            if not isinstance(endpoint_config, dict):
+                logger.warning(
+                    "Invalid endpoint configuration type '%s'; falling back to default endpoint for SITL.",
+                    type(endpoint_config).__name__,
+                )
+                master_endpoint_args = default_endpoint_args
+            else:
+                # Start with defaults, override only known fields, ignore unknown keys
+                arg_dict = EndpointDefinition().dict()
+                for key, value in endpoint_config.items():
+                    if key in default_endpoint_args:
+                        arg_dict[key] = self._sanitize_endpoint_argument(key, value)
+                    else:
+                        logger.debug("Ignoring unknown endpoint field '%s' in settings.json.", key)
+                master_endpoint_args = EndpointDefinition(**arg_dict)
+
+        master_endpoint = Endpoint(**master_endpoint_args.dict())
+
+        arg_list = [str(firmware_path)]
         for k, v in arguments.items():
-            if k != "endpoint":
-                arg_list.append(k)
-                if v != "":
-                    arg_list.append(v)
+            if k == "endpoint":
+                continue
+            arg_list.append(str(k))
+            if v != "":
+                arg_list.append(str(v))
+
         # pylint: disable=consider-using-with
         self.ardupilot_subprocess = subprocess.Popen(
             arg_list,
@@ -492,6 +508,17 @@ class AutoPilotManager(metaclass=Singleton):
         )
 
         await self.start_mavlink_manager(master_endpoint)
+
+    def _sanitize_endpoint_argument(self, key: str, value: str) -> int | bool | str:
+        if key == "argument":
+            return int(value)
+        if key == "protected":
+            if value == "True":
+                return True
+            if value == "False":
+                return False
+            raise ValueError("Invalid value for 'protected' argument of endpoint")
+        return value
 
     async def start_mavlink_manager(self, device: Endpoint) -> None:
         for endpoint in self.autopilot_default_endpoints:
